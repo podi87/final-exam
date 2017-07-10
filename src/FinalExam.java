3af7a220ccc9d511a766a188b74582a7305ee8fa
Andras Podobni <podobni.andras@gmail.com>
2017-07-10 09:32:01 +0200

    create project

---
+++ src/FinalExam.java
@@ -0,0 +1,8 @@
+/**
+ * Created by Asus on 2017-07-10.
+ */
+public class FinalExam {
+  public static void main(String[] args) {
+
+  }
+}
---
+++ src/SumEven.java
@@ -0,0 +1,13 @@
+/**
+ * Created by Asus on 2017-07-10.
+ */
+public class SumEven {
+
+  public int sumEvenNumbers(int [] inputArray) {
+    int sum = 0;
+    for(int i = 0; i < inputArray.length; i++) {
+      if (in)
+    }
+
+  }
+}
